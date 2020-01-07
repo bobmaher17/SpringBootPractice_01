315e5563f2f5b0e698d2d75f98cff65863906b9f
Maherdika Erlambang <memerlambang@gmail.com>
2020-01-08 01:54:20 +0700

    Initial Commit

---
+++ src/test/java/com/practice/firstApp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.firstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
